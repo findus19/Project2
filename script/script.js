8fd22b13a6f627a6a9f587baf3cbc4abb486c74f
findus19 <ivanexs@mail.ru>
2020-02-25 19:48:06 +0300

    day22

--- script/script.js
+++ script/script.js
@@ -300,22 +300,6 @@ window.addEventListener('DOMContentLoaded', () =>{
 
     imgContainer();
 
-    const regExp = () => {
-        let inputText = document.querySelectorAll('input[type="text"]');
-
-        inputText.forEach(item => {
-            item.addEventListener('input', () =>{
-                let inputValue = item.value;
-                let reg = /\D/;
-                if(reg.test(inputValue)){
-                    inputValue = inputValue.replace(reg, '');
-                    item.value = inputValue;
-                };
-            });
-        });
-    };
-    regExp();
-
     //калькулятор
     const calc = (price = 100) => {
         const calcBlock = document.querySelector('.calc-block'),
@@ -324,41 +308,54 @@ window.addEventListener('DOMContentLoaded', () =>{
             calcDay = document.querySelector('.calc-day'),
             calcCount = document.querySelector('.calc-count'),
             totalValue = document.getElementById('total');
+            
+        const calcul = document.querySelector('#calc');
+        calcul.addEventListener('input', e => {
+            const target = e.target;
+            if(e.target.matches('input')) {
+                target.value = target.value.replace('/[^\D]/');
+            }
+        });
 
-            const countSum = () => {
-                let total = 0,
-                    countValue = 1,
-                    dayValue = 1;
-                const typeValue = calcType.options[calcType.selectedIndex].value;
-                let squareValue = +calcSquare.value;
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            let squareValue = +calcSquare.value;
 
-                if(calcCount.value > 1){
-                    countValue += (calcCount.value -1)  / 10;
-                }
-
-                if(calcDay.value && calcDay.value < 5){
-                    dayValue *= 2;
-                } else if (calcDay.value && calcDay.value < 10){
-                    dayValue *= 1.5;
-                } 
-
-                if(typeValue && squareValue){
-                    total = price * typeValue * squareValue * countValue * dayValue;
-                }
-                 
-                totalValue.textContent = total;
+            if(calcCount.value > 1){
+                countValue += (calcCount.value -1)  / 10;
             }
 
-            calcBlock.addEventListener('change', (event) => {
-                const target = event.target
-                /* if(target === calcType || target === calcSquare || 
-                    target === calcDay || target === calcCount){
-                    countSum();
-                } */
-                if(target.matches('select') || target.matches('input')){
-                    countSum();
+            if(calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10){
+                dayValue *= 1.5;
+            } 
+
+            if(typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+            
+            totalValue.textContent = total;
+            
+        }
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target
+            /* if(target === calcType || target === calcSquare || 
+                target === calcDay || target === calcCount){
+                countSum();
+            } */
+            if(target.matches('select') || target.matches('input')){
+                let interval = setInterval(countSum, 10000);
+                if (interval >= totalValue.textContent) {
+                    totalValue.textContent = 1100
+                    return;
                 }
-            })
+            }
+        })
     };
 
     calc(100);
